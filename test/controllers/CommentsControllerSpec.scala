a00409d0a0968ebac5e7affb7cb9cf8ca1c5f557
Stephan Bauer <berlin.stephan@gmail.com>
2018-04-29 11:15:13 +0200

    test: api delivers uncomplete data

--- test/controllers/CommentsControllerSpec.scala
+++ test/controllers/CommentsControllerSpec.scala
@@ -4,7 +4,7 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.libs.ws.WSClient
+import play.api.libs.ws.{WSClient, WSResponse}
 import play.api.mvc.Action
 import play.api.mvc.Results._
 import play.api.routing.Router
@@ -23,20 +23,38 @@ class CommentsControllerSpec extends PlaySpec with GuiceOneServerPerSuite {
           Action {
             Ok("""{"username":"Alice"}""")
           }
+        case GET(p"/mocked-api/users/2") =>
+          Action {
+            Ok("""{}""")
+          }
       })
       .build()
   }
 
   "CommentsController.comments" should {
-    "deliver commentator's username and the important content" in {
-      val wsClient = app.injector.instanceOf[WSClient]
-      val commentsUrl = s"http://localhost:$port/comments"
-      val response = await(wsClient.url(s"$commentsUrl/1").get())
+    "deliver commentator's username and the important content when username is given" in {
+      val response = requestFor(1)
 
       response.status mustBe OK
       response.contentType mustBe "text/html; charset=UTF-8"
       response.body must include("Alice")
       response.body must include("Important Content")
     }
+
+    "deliver the important content when username is not given" in {
+      val response = requestFor(2)
+
+      response.status mustBe OK
+      response.contentType mustBe "text/html; charset=UTF-8"
+      response.body must include("Important Content")
+    }
   }
+
+  private def requestFor(userId: Int): WSResponse = {
+    val wsClient = app.injector.instanceOf[WSClient]
+    val commentsUrl = s"http://localhost:$port/comments/$userId"
+
+    await(wsClient.url(commentsUrl).get())
+  }
+
 }
